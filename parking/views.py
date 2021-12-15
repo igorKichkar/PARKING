ae99e8658f5e6d72d8a43fb45cb44b2a24f94243
Igor <igor_kichkar@mail.ru>
2021-12-15 11:37:01 +0200

    Removed unnecessary conditions

--- parking/views.py
+++ parking/views.py
@@ -29,70 +29,67 @@ class LoginUser(LoginView):
 def main(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    else:
-        if request.method == "POST":
-            form = ReservationFormStart(request.POST)
-            if form.is_valid():
-                day = form.cleaned_data['data']
-                place_number = form.cleaned_data['parking_number']
-                return redirect('parking_manager', day=day, place_number=place_number)
+    if request.method == "POST":
+        form = ReservationFormStart(request.POST)
+        if form.is_valid():
+            day = form.cleaned_data['data']
+            place_number = form.cleaned_data['parking_number']
+            return redirect('parking_manager', day=day, place_number=place_number)
 
-        else:
-            form = ReservationFormStart
-        return render(request, 'parking/main.html', context={'form': form})
+    else:
+        form = ReservationFormStart
+    return render(request, 'parking/main.html', context={'form': form})
 
 
 def edit_space(request):
     if not request.user.is_authenticated and hasattr(request.user, "status"):
         return redirect('login')
+    if request.method == "POST":
+        form = NewParkingSpace(request.POST)
+        if form.is_valid():
+            space = form.save(commit=False)
+            space.save()
+            return redirect('edit_space')
     else:
-        if request.method == "POST":
-            form = NewParkingSpace(request.POST)
-            if form.is_valid():
-                space = form.save(commit=False)
-                space.save()
-                return redirect('edit_space')
-        else:
-            form = NewParkingSpace
+        form = NewParkingSpace
 
-        context = {
-            'space': ParkingSpace.objects.all(),
-            'form': form,
-        }
-        return render(request, 'parking/edit_space.html', context=context)
+    context = {
+        'space': ParkingSpace.objects.all(),
+        'form': form,
+    }
+    return render(request, 'parking/edit_space.html', context=context)
 
 
 def parking_manager(request, day, place_number):
     if not request.user.is_authenticated:
         return redirect('login')
+    parking_number = ParkingSpace.objects.get(number=place_number)
+    info = ParkingReservation.objects.filter(data=day, parking_number=parking_number)
+    data = {'user': request.user}
+    if request.method == "POST":
+        form = ReservationFormForManager(request.POST) if request.user.status else ReservationForm(request.POST)
+        if form.is_valid():
+            start_time = form.cleaned_data['start_time']
+            end_time = form.cleaned_data['end_time']
+            user = form.cleaned_data['user'] if request.user.status else request.user
+            if start_time >= end_time:
+                messages.add_message(request, messages.INFO, 'Wrong time')
+            elif date_intersection(start_time, end_time, info):
+                messages.add_message(request, messages.INFO, 'The selected time is already booked')
+            else:
+                ParkingReservation.objects.create(parking_number=parking_number, data=day, start_time=start_time,
+                                                    end_time=end_time, user=user)
+                messages.add_message(request, messages.SUCCESS, 'Entry added successfully')
+                return redirect('parking_lots')
     else:
-        parking_number = ParkingSpace.objects.get(number=place_number)
-        info = ParkingReservation.objects.filter(data=day, parking_number=parking_number)
-        data = {'user': request.user}
-        if request.method == "POST":
-            form = ReservationFormForManager(request.POST) if request.user.status else ReservationForm(request.POST)
-            if form.is_valid():
-                start_time = form.cleaned_data['start_time']
-                end_time = form.cleaned_data['end_time']
-                user = form.cleaned_data['user'] if request.user.status else request.user
-                if start_time >= end_time:
-                    messages.add_message(request, messages.INFO, 'Wrong time')
-                elif date_intersection(start_time, end_time, info):
-                    messages.add_message(request, messages.INFO, 'The selected time is already booked')
-                else:
-                    ParkingReservation.objects.create(parking_number=parking_number, data=day, start_time=start_time,
-                                                      end_time=end_time, user=user)
-                    messages.add_message(request, messages.SUCCESS, 'Entry added successfully')
-                    return redirect('parking_lots')
-        else:
-            form = ReservationFormForManager(initial=data) if request.user.status else ReservationForm(initial=data)
-        context = {
-            'form': form,
-            'info': info,
-            'place_number': place_number,
-            'day': day,
-        }
-        return render(request, 'parking/parking_manager.html', context=context)
+        form = ReservationFormForManager(initial=data) if request.user.status else ReservationForm(initial=data)
+    context = {
+        'form': form,
+        'info': info,
+        'place_number': place_number,
+        'day': day,
+    }
+    return render(request, 'parking/parking_manager.html', context=context)
 
 
 def delete_space(request, place_number):
@@ -115,52 +112,50 @@ def delete_rezerv(request, place_number):
 def parking_lots(request):
     if not request.user.is_authenticated:
         return redirect('login')
+    if request.user.status:
+        reserves = ParkingReservation.objects.all()
     else:
-        if request.user.status:
-            reserves = ParkingReservation.objects.all()
-        else:
-            reserves = ParkingReservation.objects.filter(user=request.user)
-        context = {
-            'reserves': reserves
-        }
-        return render(request, 'parking/parking_lots.html', context=context)
+        reserves = ParkingReservation.objects.filter(user=request.user)
+    context = {
+        'reserves': reserves
+    }
+    return render(request, 'parking/parking_lots.html', context=context)
 
 
 def edit_parking_lots(request, rezerv):
     if not request.user.is_authenticated:
         redirect('login')
+    rezerv_space = ParkingReservation.objects.get(pk=rezerv)
+    data = {'parking_number': rezerv_space.parking_number, 'data': rezerv_space.data,
+            'start_time': rezerv_space.start_time, 'end_time': rezerv_space.end_time}
+    if request.method == "POST":
+        form = EditReservationFormForManager(request.POST) if request.user.status else EditReservationForm(
+            request.POST)
+        if form.is_valid():
+            rezerv_space.parking_number = form.cleaned_data['parking_number']
+            rezerv_space.data = form.cleaned_data['data']
+            rezerv_space.start_time = form.cleaned_data['start_time']
+            rezerv_space.end_time = form.cleaned_data['end_time']
+            info = ParkingReservation.objects.filter(data=rezerv_space.data,
+                                                        parking_number=rezerv_space.parking_number)
+            if request.user.status:
+                rezerv_space.user = form.cleaned_data['user']
+            else:
+                rezerv_space.user = request.user
+            if rezerv_space.start_time >= rezerv_space.end_time:
+                messages.add_message(request, messages.INFO, 'Wrong time')
+            elif date_intersection(rezerv_space.start_time, rezerv_space.end_time, info, rezerv_space):
+                messages.add_message(request, messages.INFO, 'The selected time is already booked')
+            else:
+                rezerv_space.save()
+                messages.add_message(request, messages.SUCCESS, 'Record updated successfully')
+                return redirect('parking_lots')
     else:
-        rezerv_space = ParkingReservation.objects.get(pk=rezerv)
-        data = {'parking_number': rezerv_space.parking_number, 'data': rezerv_space.data,
-                'start_time': rezerv_space.start_time, 'end_time': rezerv_space.end_time}
-        if request.method == "POST":
-            form = EditReservationFormForManager(request.POST) if request.user.status else EditReservationForm(
-                request.POST)
-            if form.is_valid():
-                rezerv_space.parking_number = form.cleaned_data['parking_number']
-                rezerv_space.data = form.cleaned_data['data']
-                rezerv_space.start_time = form.cleaned_data['start_time']
-                rezerv_space.end_time = form.cleaned_data['end_time']
-                info = ParkingReservation.objects.filter(data=rezerv_space.data,
-                                                         parking_number=rezerv_space.parking_number)
-                if request.user.status:
-                    rezerv_space.user = form.cleaned_data['user']
-                else:
-                    rezerv_space.user = request.user
-                if rezerv_space.start_time >= rezerv_space.end_time:
-                    messages.add_message(request, messages.INFO, 'Wrong time')
-                elif date_intersection(rezerv_space.start_time, rezerv_space.end_time, info, rezerv_space):
-                    messages.add_message(request, messages.INFO, 'The selected time is already booked')
-                else:
-                    rezerv_space.save()
-                    messages.add_message(request, messages.SUCCESS, 'Record updated successfully')
-                    return redirect('parking_lots')
-        else:
-            form = EditReservationFormForManager(initial=data) if request.user.status else EditReservationForm(
-                initial=data)
-        context = {
-            'form': form,
-            'rezerv_space': rezerv_space,
-            'info_user': rezerv_space.user,
-        }
-        return render(request, 'parking/edit_parking_lots.html', context=context)
+        form = EditReservationFormForManager(initial=data) if request.user.status else EditReservationForm(
+            initial=data)
+    context = {
+        'form': form,
+        'rezerv_space': rezerv_space,
+        'info_user': rezerv_space.user,
+    }
+    return render(request, 'parking/edit_parking_lots.html', context=context)
